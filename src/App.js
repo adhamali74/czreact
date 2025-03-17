0666837cb5cada0864a92bbf076af77908692993
Adham Abdelfattah <50050146+adhamali74@users.noreply.github.com>
2025-03-17 17:47:26 +0100

    Applying Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -1,44 +1,37 @@
 /** @format */
-import Greeting from "./components/Greeting";
-import Welcome from "./components/Welcome";
-import Product from "./components/Product";
-// import Button from "./components/Button";
-import Input from "./components/Input";
-import Form from "./components/Form";
+
+import { useState } from "react";
+
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  let element;
+  // if (isLoggedIn) {
+  //   element = <h1>Welcome back!</h1>;
+  // } else {
+  //   element = <h1>Please log in.</h1>;
+  // }
+
+  isLoggedIn
+    ? (element = <h1>Welcome back!</h1>)
+    : (element = <h1>Please log in.</h1>);
   return (
     <div className="App">
-      {/* /* <Greeting />
-      <Welcome name="Adham" age="28" hobby="football" />
-      <Product
-        name="Nike Shoes"
-        description="High-performance shoes"
-        price="1150"
-        buttonTitle="Add to Cart"
-      />
-      <Product
-        name="Adidas Shoes"
-        description="Medium-performance shoes"
-        price="250"
-        buttonTitle="Buy Now"
-      />
-      <Product
-        name="Puma Shoes"
-        description="Low-performance shoes"
-        price="150"
-        buttonTitle="Add to Basket"
-      />
-      {/* <Button></Button> */}
-      {/* <Input label="Name" type="text" placeholder="Type your Name"></Input>
-      <Input
-        label="Password"
-        type="password"
-        placeholder="Insert your password"
-        btnName="Submit"
-      // ></Input> */
-      }{" "}
-    
-      <Form></Form>
+      <button
+        onClick={() => {
+          setIsLoggedIn(true);
+        }}
+      >
+        Login
+      </button>
+      {element}
+      <br></br>
+      <button
+        onClick={() => {
+          setIsLoggedIn(false);
+        }}
+      >
+        logout
+      </button>
     </div>
   );
 }
