de3682a74461adeca255303b00bbe3dc890d2c24
Adham Abdelfattah <50050146+adhamali74@users.noreply.github.com>
2025-03-17 19:46:33 +0100

    Cond.rendering with ternary operator

--- src/App.js
+++ src/App.js
@@ -4,16 +4,13 @@ import { useState } from "react";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  let element;
+
   // if (isLoggedIn) {
   //   element = <h1>Welcome back!</h1>;
   // } else {
   //   element = <h1>Please log in.</h1>;
   // }
 
-  isLoggedIn
-    ? (element = <h1>Welcome back!</h1>)
-    : (element = <h1>Please log in.</h1>);
   return (
     <div className="App">
       <button
@@ -23,7 +20,7 @@ function App() {
       >
         Login
       </button>
-      {element}
+      {isLoggedIn ? <h1>Welcome back!</h1> : <h1>Please log in.</h1>}
       <br></br>
       <button
         onClick={() => {
